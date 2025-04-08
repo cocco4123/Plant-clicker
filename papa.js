cfc4d03a4269e5b183480d2dce53747831837013
cocco4123 <155199739+cocco4123@users.noreply.github.com>
2025-04-08 18:46:51 -0300

    Se agrega el proyecto

---
+++ papa.js
@@ -0,0 +1,10 @@
+let countEL = document.getElementById("count")
+let papaEL = document.getElementById("papa")
+let count = 0
+
+function papa(){
+    count += 1
+    countEL.textContent = count
+}
+
+
