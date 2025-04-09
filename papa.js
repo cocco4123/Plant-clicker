6cdbc209d41af3e39d6402ea74a07dcf09e2133e
cocco4123 <josebarcelodawi@gmail.com>
2025-04-09 19:31:08 -0300

    update 1.2
    mejora de clicks lista, capacidad infinita y alert de error en caso de no tener necesario (falta hacer modal)

--- papa.js
+++ papa.js
@@ -1,18 +1,33 @@
-let countEL = document.getElementById("count")
-let papaEL = document.getElementById("papa")
-let count = 0
-let pointsonclick = 1
-let currentupdate = 0
+let countEL = document.getElementById("count");
+let papaEL = document.getElementById("papa");
+let count = 0;
+let pointsOnClick = 1;
+let upgradeLevel = 0;
 
-function upgradeclicks(){
-currentupdate + 
- }
-
-function papa(){
-    count += pointsonclick
-    countEL.textContent = count
+function getUpgradeCost() {
+    return Math.floor(50 * Math.pow(1.15, upgradeLevel)); 
 }
 
+function papa() {
+    count += pointsOnClick;
+    updateUI();
+}
 
+function buyUpgrade() {
+    let cost = getUpgradeCost(); 
+    if (count >= cost) {
+        count -= cost;            
+        upgradeLevel++;          
+        pointsOnClick++;
+        updateUI();
+    } else {
+        alert("You need more points to buy this. You need " + (cost - count));
 
+    }
+}
 
+function updateUI() {
+    document.getElementById("count").innerText = count;
+    document.getElementById("upgrade-cost").innerText = getUpgradeCost();
+    document.getElementById("per-click").innerText = pointsOnClick;
+}
